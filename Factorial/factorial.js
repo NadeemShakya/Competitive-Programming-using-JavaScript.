79a7046c6a8090920529ad8af20a155b95ca8d54
Nadeem Shakya <shakya.nadim12@gmail.com>
2019-04-16 11:00:36 +0545

    Add files via upload

---
+++ Factorial/factorial.js
@@ -0,0 +1,10 @@
+function factorial(number) {
+    if(number === 0 || number === 1) {
+        return 1;
+    }else {
+        return number * factorial(number - 1);
+    }
+}
+
+let output = factorial(0);
+console.log(output);
